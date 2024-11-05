20078b2b6d713dc7427d70587a718867e6d5d471
linClubs <904798869@qq.com>
2024-11-05 16:19:33 +0800

    add finger control

--- mocap_pkg/src/h1_arm_hand.cpp
+++ mocap_pkg/src/h1_arm_hand.cpp
@@ -24,9 +24,9 @@ H1Teleop::H1Teleop() : Node("h1_node")
   curr_head_q_ = std::vector<float>(4, 0.);
   
   // inspire hand config
-  // serial1_ = std::make_shared<SerialPort>("/dev/ttyUSB0", B115200);
-  // lefthand = std::make_shared<inspire::InspireHand>(serial1_, 2); // ID 2
-  // righthand = std::make_shared<inspire::InspireHand>(serial2_, 1); // ID 2
+  serial1_ = std::make_shared<SerialPort>("/dev/ttyUSB0", B115200);
+  lefthand = std::make_shared<inspire::InspireHand>(serial1_, 2); // ID 2
+  righthand = std::make_shared<inspire::InspireHand>(serial2_, 1); // ID 2
 
 
   // pub
@@ -44,34 +44,41 @@ H1Teleop::H1Teleop() : Node("h1_node")
 }
 
 void H1Teleop::timer_callback()
-{   
+{   // 1 update data
     // 34, 8 + 1(left_hand) + 12(left_fingers) + 1(right_hand) + 12(right_fingers)
     // upJoints[[8], [21]] = 0, upJoints[9->20](left_thumb 9->12, left_index->little 13->20) 
-    // std::cout << "arm:  " ;
     auto upJoints = mocap->getJoints();
-    // for (int i = 0; i < 8; ++i)
-    // {
-    //   std::cout << upJoints[i] << "  ";
-    // }
+
+    // 2 finger data process
+    // 2.1 left_finger, qcmd[litte, ring, middle, index, thumb_1, thumb_2]
+    //  qcmd[0, 1, 2, 3] -> upJoints[20, 18, 16, 14],  // little -> index    
+    qcmd(4) = 1.2 -  upJoints[9];   // 大拇指
+    qcmd(5) = 0.5 -  upJoints[10];   // 大拇指
+    for(int i = 0; i < 4; i++)
+    { 
+      qcmd(i) =1.7 - upJoints[20 - i * 2]; 
+    }
+
+    // 2.2 right_finger
+    // qcmd[10, 11] -> upJoints[22, 23, 24, 25]
+    qcmd(10) = 1.2 -  upJoints[22];   // 大拇指
+    qcmd(11) = 0.5 -  upJoints[23];   // 大拇指
+    // qcmd[6, 7, 8, 9] -> upJoints[33, 31, 29, 27]
+    for(int i = 0; i < 4; i++)
+    { 
+      qcmd(6 + i) =1.7 - upJoints[33 - i * 2]; 
+    }
     
-    // std::cout << std::endl;
     // 手指部分 更新qcmd
-    // lefthand->SetPosition(qcmd.block<6, 1>(0, 0));
-    // righthand->SetPosition(qcmd.block<6, 1>(6, 0));
+    lefthand->SetPosition(qcmd.block<6, 1>(0, 0));
+    righthand->SetPosition(qcmd.block<6, 1>(6, 0));
 
     // 手臂处理
     for(int i = 0; i < 8; ++i)
     {
       next_arm_q_[i+12] = upJoints[i]; 
     }
-    
-    // if(first_frame_)
-    // {
-    //   first_frame_ = false;
-    //   curr_arm_q_ = next_arm_q_;
-    //   curr_head_q_ = next_head_q_;
-    //   return;
-    // }
+
     // 4 手臂unitree消息 20维
     unitree_go::msg::MotorCmds bodyCmdsMsg;
     unitree_go::msg::MotorCmd bodyCmd;
@@ -84,33 +91,17 @@ void H1Teleop::timer_callback()
     }
     bodyCmdsMsg.cmds[9].q = 1.0;
 
-    // 5.2 arm手臂数据插值 mean.at(i)每次插值数值，插值数
-    // std::vector<float>diff(20, 0.0);
-    // int INSERT_NUM = 1
-    // ;
-    // for (int i = 12; i <= 19; ++i) 
-    // {
-    //     diff.at(i) = (next_arm_q_.at(i) - curr_arm_q_.at(i)) / INSERT_NUM;
-    // }
-
     std::cout << "arm  : ";
-    // for(int i = 0; i < INSERT_NUM; ++i)
-    { 
-      // arm_joints只有8个数字，从left开始
-      for (int j = 12; j < 20; ++j) 
-      {
-        // bodyCmdsMsg.cmds[arm_joints.at(j-12)].q = curr_arm_q_.at(j) + diff.at(j);  // 未填充字段保持之前动作 
-        bodyCmdsMsg.cmds[arm_joints.at(j-12)].q = next_arm_q_.at(j) ;
-        std::cout << next_arm_q_.at(j) << "  ";
-      }
-      std::cout << std::endl;
-      pub_arm_->publish(bodyCmdsMsg);
-      std::this_thread::sleep_for(std::chrono::milliseconds(1));
-
+    for (int j = 12; j < 20; ++j) 
+    {
+      bodyCmdsMsg.cmds[arm_joints.at(j-12)].q = next_arm_q_.at(j) ;
+      std::cout << next_arm_q_.at(j) << "  ";
     }
-    // curr_arm_q_ = next_arm_q_;
+    std::cout << std::endl;
+    pub_arm_->publish(bodyCmdsMsg);
+   
+   // std::this_thread::sleep_for(std::chrono::milliseconds(1));
 };
-
 
 int main(int argc, char** argv)
 { 
